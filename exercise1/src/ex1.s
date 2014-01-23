6e4baaf0f0549402f7011c63eeccad17005fc503
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-01-23 21:54:09 +0100

    corrected indentation in handout files

--- exercise1/src/ex1.s
+++ exercise1/src/ex1.s
@@ -7,69 +7,69 @@
 // This table contains addresses for all exception handlers
 //
 /////////////////////////////////////////////////////////////////////////////
-	
-.section .vectors
-	
-	.long   stack_top               /* Top of Stack                 */
-	.long   _reset                  /* Reset Handler                */
-	.long   dummy_handler           /* NMI Handler                  */
-	.long   dummy_handler           /* Hard Fault Handler           */
-	.long   dummy_handler           /* MPU Fault Handler            */
-	.long   dummy_handler           /* Bus Fault Handler            */
-	.long   dummy_handler           /* Usage Fault Handler          */
-	.long   dummy_handler           /* Reserved                     */
-	.long   dummy_handler           /* Reserved                     */
-	.long   dummy_handler           /* Reserved                     */
-	.long   dummy_handler           /* Reserved                     */
-	.long   dummy_handler           /* SVCall Handler               */
-	.long   dummy_handler           /* Debug Monitor Handler        */
-	.long   dummy_handler           /* Reserved                     */
-	.long   dummy_handler           /* PendSV Handler               */
-	.long   dummy_handler           /* SysTick Handler              */
 
-	/* External Interrupts */
-	.long   dummy_handler
-	.long   gpio_handler            /* GPIO even handler */
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   gpio_handler            /* GPIO odd handler */
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
-	.long   dummy_handler
+.section .vectors
+
+    .long   stack_top               /* Top of Stack                 */
+    .long   _reset                  /* Reset Handler                */
+    .long   dummy_handler           /* NMI Handler                  */
+    .long   dummy_handler           /* Hard Fault Handler           */
+    .long   dummy_handler           /* MPU Fault Handler            */
+    .long   dummy_handler           /* Bus Fault Handler            */
+    .long   dummy_handler           /* Usage Fault Handler          */
+    .long   dummy_handler           /* Reserved                     */
+    .long   dummy_handler           /* Reserved                     */
+    .long   dummy_handler           /* Reserved                     */
+    .long   dummy_handler           /* Reserved                     */
+    .long   dummy_handler           /* SVCall Handler               */
+    .long   dummy_handler           /* Debug Monitor Handler        */
+    .long   dummy_handler           /* Reserved                     */
+    .long   dummy_handler           /* PendSV Handler               */
+    .long   dummy_handler           /* SysTick Handler              */
+
+    /* External Interrupts */
+    .long   dummy_handler
+    .long   gpio_handler            /* GPIO even handler */
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   gpio_handler            /* GPIO odd handler */
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+    .long   dummy_handler
+
 
 .section .text
-
 /////////////////////////////////////////////////////////////////////////////
 //
 // Reset handler
@@ -80,8 +80,8 @@
 .type   _reset, %function
 .thumb_func
 _reset:
-	b .  // do nothing
-	
+b .  // do nothing
+
 /////////////////////////////////////////////////////////////////////////////
 //
 // GPIO handler
@@ -90,11 +90,14 @@ _reset:
 /////////////////////////////////////////////////////////////////////////////
 .thumb_func
 gpio_handler:
-	b .  // do nothing
-	
-/////////////////////////////////////////////////////////////////////////////
-	
-.thumb_func
-dummy_handler:  
-        b .  // do nothing
+b .  // do nothing
 
+/////////////////////////////////////////////////////////////////////////////
+//
+// Some dummy handler
+//
+/////////////////////////////////////////////////////////////////////////////
+
+.thumb_func
+dummy_handler:
+b . // do nothing
