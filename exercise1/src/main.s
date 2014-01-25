1c0bac1df462c53124506911b39434256fb09c9c
Mathias Ose <mathias.ose@gmail.com>
2014-01-25 15:50:46 +0100

    Moved init code to reset handler, added startup led test sequence

--- exercise1/src/main.s
+++ exercise1/src/main.s
@@ -80,7 +80,59 @@
 .type   _reset, %function
 .thumb_func
 _reset:
-    b init  // do nothing
+    // Enable clock for GPIO
+    LDR R4, =CMU_BASE
+    LDR R5, [R4, #CMU_HFPERCLKEN0]
+    MOV R6, #1
+    LSL R6, R6, #CMU_HFPERCLKEN0_GPIO
+    ORR R5, R5, R6
+    STR R5, [R4, #CMU_HFPERCLKEN0]
+    // Set high drive strength
+    LDR R4, =GPIO_PA_BASE
+    MOV R7, #0x2
+    STR R7, [R4, #GPIO_CTRL]
+    // Set pins 8-15 of port A for output
+    LDR R7, =0x55555555
+    STR R7, [R4, #GPIO_MODEH]
+    // Test LED, Startup sequence
+    LDR R5, =0x7FFF
+    LDR R6, =0xBFFF
+    LDR R7, =0xDFFF
+    LDR R8, =0xEFFF
+    LDR R9, =0xF7FF
+    LDR R10, =0xFBFF
+    LDR R11, =0xFDFF
+    LDR R12, =0xFEFF
+    STR R5, [R4, #GPIO_DOUT]
+    BL dowait
+    STR R6, [R4, #GPIO_DOUT]
+    BL dowait
+    STR R7, [R4, #GPIO_DOUT]
+    BL dowait
+    STR R8, [R4, #GPIO_DOUT]
+    BL dowait
+    STR R9, [R4, #GPIO_DOUT]
+    BL dowait
+    STR R10, [R4, #GPIO_DOUT]
+    BL dowait
+    STR R11, [R4, #GPIO_DOUT]
+    BL dowait
+    STR R12, [R4, #GPIO_DOUT]
+    BL dowait
+    LDR R1, =0xFFFF
+    STR R1, [R4, #GPIO_DOUT]
+    BL dowait
+    LDR R1, =0x00FF
+    STR R1, [R4, #GPIO_DOUT]
+    BL dowait
+    LDR R1, =0xFFFF
+    STR R1, [R4, #GPIO_DOUT]
+    BL dowait
+    LDR R1, =0x00FF
+    STR R1, [R4, #GPIO_DOUT]
+    BL dowait
+    LDR R1, =0xFFFF
+    STR R1, [R4, #GPIO_DOUT]
 
 /////////////////////////////////////////////////////////////////////////////
 //
@@ -106,25 +158,19 @@ dummy_handler:
 // Init
 .thumb_func
 init:
-    // Enable clock for GPIO
-    LDR R4, =CMU_BASE
-    LDR R5, [R4, #CMU_HFPERCLKEN0]
-    MOV R6, #1
-    LSL R6, R6, #CMU_HFPERCLKEN0_GPIO
-    ORR R5, R5, R6
-    STR R5, [R4, #CMU_HFPERCLKEN0]
-    // Set high drive strength
-    LDR R4, =GPIO_PA_BASE
-    MOV R7, #0x2
-    STR R7, [R4, #GPIO_CTRL]
-    // Set pins 8-15 of port A for output
-    LDR R7, =0x55555555
-    STR R7, [R4, #GPIO_MODEH]
-    // Test LED
-    LDR R5, =0x0
-    STR R5, [R4, #GPIO_DOUT]
+   B .
 
 // Main loop
 .thumb_func
 main:
     B main
+
+
+ // Helper sleepfunction
+.thumb_func
+dowait:
+    LDR R2, =0x100000
+dowaitloop:
+    SUBS R2, #1
+    BNE dowaitloop
+    BX lr
