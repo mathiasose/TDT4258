4437bf8de5bcab24228647989ec1ced3a9814096
Mathias Ose <mathias.ose@gmail.com>
2014-01-25 21:02:30 +0100

    same behavior, attempts to conserve energy by sleeping and waiting for interrupts. using register aliases.

--- exercise1/src/main.s
+++ exercise1/src/main.s
@@ -80,89 +80,53 @@
 .type   _reset, %function
 .thumb_func
 _reset:
-    // Enable clock for GPIO
-    LDR R4, =CMU_BASE
-    LDR R5, [R4, #CMU_HFPERCLKEN0]
-    MOV R6, #1
-    LSL R6, R6, #CMU_HFPERCLKEN0_GPIO
-    ORR R5, R5, R6
-    STR R5, [R4, #CMU_HFPERCLKEN0]
-    // Set high drive strength
+    // aliases
+    GPIO_O .req R4
+    GPIO_I .req R5
+    GPIO .req R6
+    T1 .req R7
+    T2 .req R8
+    T3 .req R9
+    // do not change these registers
     LDR R4, =GPIO_PA_BASE
-    MOV R7, #0x2
-    STR R7, [R4, #GPIO_CTRL]
+    LDR R5, =GPIO_PC_BASE
+    LDR R6, =GPIO_BASE
+    // Enable clock for GPIO
+    LDR T3, =CMU_BASE
+    LDR T1, [T3, #CMU_HFPERCLKEN0]
+    MOV T2, #1
+    LSL T2, T2, #CMU_HFPERCLKEN0_GPIO
+    ORR T1, T1, T2
+    STR T1, [T3, #CMU_HFPERCLKEN0]
+    // Set high drive strength
+    MOV T1, #0x2
+    STR T1, [GPIO_O, #GPIO_CTRL]
     // Set pins 8-15 of port A for output
-    LDR R7, =0x55555555
-    STR R7, [R4, #GPIO_MODEH]
+    LDR T1, =0x55555555
+    STR T1, [GPIO_O, #GPIO_MODEH]
     BL led_test
     // Set pins 0-7 of port C for input
-    LDR R4, =GPIO_PC_BASE
-    LDR R7, =0x33333333
-    STR R7, [R4, #GPIO_MODEL]
-    LDR R7, =0xFF
-    STR R7, [R4, #GPIO_DOUT]
-
-// Main loop
-.thumb_func
-main:
-    LDR R4, =GPIO_PA_BASE //output
-    LDR R5, =GPIO_PC_BASE //input
-    LDR R5, [R5, #GPIO_DIN]
-    MOV R7, #8
-    LSL R5, R5, R7 // shift the 8 lsb from input to use as 8 msb for output
-    STR R5, [R4, #GPIO_DOUT]
-    B main
-
-
- // Helper sleepfunction
-.thumb_func
-dowait:
-    LDR R2, =0x100000
-dowaitloop:
-    SUBS R2, #1
-    BNE dowaitloop
-    BX lr
-
-.thumb_func
-led_test:
-    PUSH {lr}
-    // Test LEDs sequentially
-    LDR R5, =0x7FFF
-    LDR R6, =0xBFFF
-    LDR R7, =0xDFFF
-    LDR R8, =0xEFFF
-    LDR R9, =0xF7FF
-    LDR R10, =0xFBFF
-    LDR R11, =0xFDFF
-    LDR R12, =0xFEFF
-    STR R5, [R4, #GPIO_DOUT]
-    BL dowait
-    STR R6, [R4, #GPIO_DOUT]
-    BL dowait
-    STR R7, [R4, #GPIO_DOUT]
-    BL dowait
-    STR R8, [R4, #GPIO_DOUT]
-    BL dowait
-    STR R9, [R4, #GPIO_DOUT]
-    BL dowait
-    STR R10, [R4, #GPIO_DOUT]
-    BL dowait
-    STR R11, [R4, #GPIO_DOUT]
-    BL dowait
-    STR R12, [R4, #GPIO_DOUT]
-    BL dowait
-    // Blink all LEDs twice
-    LDR R0, =0xFFFF
-    LDR R1, =0x00FF
-    STR R1, [R4, #GPIO_DOUT]
-    BL dowait
-    STR R0, [R4, #GPIO_DOUT]
-    BL dowait
-    STR R1, [R4, #GPIO_DOUT]
-    BL dowait
-    STR R0, [R4, #GPIO_DOUT]
-    // Return function, LEDs are now off 
-    POP {pc}
+    LDR T1, =0x33333333
+    STR T1, [GPIO_I, #GPIO_MODEL]
+    LDR T1, =0xFF
+    STR T1, [GPIO_I, #GPIO_DOUT]
+    // EXTIP
+    LDR T1, =0x22222222
+    STR T1, [GPIO, #GPIO_EXTIPSELL]
+    // Configure RISE/FALL
+    LDR T1, =0xFF
+    STR T1, [GPIO, #GPIO_EXTIRISE]
+    STR T1, [GPIO, #GPIO_EXTIFALL]
+    STR T1, [GPIO, #GPIO_IEN]
+    // Enable NVIC for GPIO
+    LDR T1, =0x802
+    LDR T2, =ISER0
+    STR T1, [T2]
+    // Enable automatic deep sleep after return from interrupt handler
+    MOV T1, #6
+    LDR T2, =SCR
+    STR T1, [GPIO]
+    WFI
     
 /////////////////////////////////////////////////////////////////////////////
 //
@@ -172,7 +136,71 @@ led_test:
 /////////////////////////////////////////////////////////////////////////////
 .thumb_func
 gpio_handler:
-    b .  // do nothing
+    //Clear interrupt flag
+    LDR T1, =0xff
+    LDR GPIO_O, =GPIO_PC_BASE
+    STR T1, [GPIO_O, #GPIO_IFC]
+    //Perform actual signal processing
+    LDR GPIO_O, =GPIO_PA_BASE //output
+    LDR GPIO_I, =GPIO_PC_BASE //input
+    LDR GPIO_I, [GPIO_I, #GPIO_DIN]
+    MOV T1, #8
+    LSL GPIO_I, GPIO_I, T1 // shift the 8 lsb from input to use as 8 msb for output
+    STR GPIO_I, [GPIO_O, #GPIO_DOUT]
+    BX lr
+
+ // Helper sleepfunction
+.thumb_func
+dowait:
+    LDR T1, =0x50000
+dowaitloop:
+    SUBS T1, #1
+    BNE dowaitloop
+    BX lr
+
+.thumb_func
+led_test:
+    PUSH {lr}
+    // Test LEDs sequentially
+    LDR T1, =0x7FFF
+    STR T1, [GPIO_O, #GPIO_DOUT]
+    BL dowait
+    LDR T1, =0xBFFF
+    STR T1, [GPIO_O, #GPIO_DOUT]
+    BL dowait
+    LDR T1, =0xDFFF
+    STR T1, [GPIO_O, #GPIO_DOUT]
+    BL dowait    
+    LDR T1, =0xEFFF
+    STR T1, [GPIO_O, #GPIO_DOUT]
+    BL dowait
+    LDR T1, =0xF7FF
+    STR T1, [GPIO_O, #GPIO_DOUT]
+    BL dowait
+    LDR T1, =0xFBFF
+    STR T1, [GPIO_O, #GPIO_DOUT]
+    BL dowait
+    LDR T1, =0xFDFF
+    STR T1, [GPIO_O, #GPIO_DOUT]
+    BL dowait
+    LDR T1, =0xFEFF
+    STR T1, [GPIO_O, #GPIO_DOUT]
+    BL dowait
+    // Blink all LEDs twice
+    LDR T2, =0xFFFF
+    LDR T1, =0x00FF
+    STR T1, [GPIO_O, #GPIO_DOUT]
+    BL dowait
+    BL dowait
+    STR T2, [GPIO_O, #GPIO_DOUT]
+    BL dowait
+    BL dowait
+    STR T1, [GPIO_O, #GPIO_DOUT]
+    BL dowait
+    BL dowait
+    STR T2, [GPIO_O, #GPIO_DOUT]
+    // Return function, LEDs are now off 
+    POP {pc}
 
 /////////////////////////////////////////////////////////////////////////////
 //
