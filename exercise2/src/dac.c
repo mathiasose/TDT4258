ae057c8e4d29829cddd855ffa8c9accf0dd7ac11
Mathias Ose <mathias.ose@gmail.com>
2014-03-06 16:01:58 +0100

    idles at 1.9uA

--- exercise2/src/dac.c
+++ exercise2/src/dac.c
@@ -11,8 +11,8 @@ void setupDAC() {
 }
 
 void disableDAC() {
-    *CMU_HFPERCLKEN0 &= ~(1 << 17);
     *DAC0_CTRL = 0;
     *DAC0_CH0CTRL = 0;
     *DAC0_CH1CTRL = 0;
+    *CMU_HFPERCLKEN0 &= ~(1 << 17);
 }
