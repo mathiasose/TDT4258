ef48128e4a7f716b9122d5cf4e296ea9b1b12a73
Mathias Ose <mathias.ose@gmail.com>
2014-03-06 15:52:44 +0100

    prototypes for prev. commit

--- exercise2/src/prototypes.h
+++ exercise2/src/prototypes.h
@@ -8,6 +8,9 @@ void disableDAC(void);
 
 void setupNVIC(void);
 void setupGPIO(void);
+void GPIO_interrupt_clear(void);
+void GPIO_LED(void);
+int map_input(void);
 
 void setupSleep(int arg);
 void wfi(void);
