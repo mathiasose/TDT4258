896afa76cbf39c0a9b82f96104f51e6a2eb18fa8
Mathias Ose <mathias.ose@gmail.com>
2014-03-07 14:42:34 +0100

    wrote some more, added plot of sample sine curve

--- exercise2/support/music.py
+++ exercise2/support/music.py
@@ -1,7 +1,13 @@
 import random
 import os
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from pylab import *
+
 from math import *
 from datetime import datetime
+
 from scale import scale
 
 def sine_samples(frequency=440.0, framerate=44100):
@@ -160,3 +166,17 @@ if __name__ == "__main__":
 
     with open(os.path.join(directory, '../src/music.c'), 'w') as c_file:
 	c_file.write(c)
+
+    # plot for report
+    plt.plot(sine_samples(scale["A4"]), '.')
+    x1,x2,y1,y2 = plt.axis()
+    plt.axis((x1,x2,0,0x100))
+    plt.axes = plt.gca()
+    plt.xlabel('Samples')
+    plt.title('Samples producing A4')
+    plt.ylabel('Amplitude (hexadecimal)')
+    plt.axes.get_yaxis().set_major_locator(ticker.MultipleLocator(0x10))
+    plt.axes.get_yaxis().set_major_formatter(ticker.FormatStrFormatter("%x"))
+#    plt.show()
+    path = os.path.join(directory, '../report/img/A4.png') 
+    plt.savefig(path, bbox_inches='tight')
