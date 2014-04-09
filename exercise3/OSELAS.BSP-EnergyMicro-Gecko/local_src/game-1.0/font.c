a614b59e4bef6435a6567f4b8a3fab10341d5c84
oyvindrobertsen <oyvind@arcticstyle.net>
2014-04-09 17:36:43 +0200

    font reading

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/font.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/font.c
@@ -18,9 +18,46 @@ int init_fonts()
 
 int path_to_font(char* path, font* fontp) 
 {
-    FILE* f_ptr = fopen(path, "rb");
-    if (f_ptr == NULL) {
+    FILE* f_ptr; 
+    bitmap_fileheader bfh;
+    bitmap_infoheader bih;
+    // Open file
+    if ((f_ptr = fopen(path, "rb")) == NULL) {
         printf("Error: failed to open file.\n");
         return EXIT_FAILURE;
-    } 
+    }
+
+    // Read file header
+    fread(&bfh, sizeof(bitmap_fileheader), 1, f_ptr);
+    if (bfh.bf_type != 0x4D42) {
+        printf("Error: not a .bmp file\n");
+        fclose(f_ptr);
+        return EXIT_FAILURE;
+    }
+
+    // Read info header
+    fread(&bih, sizeof(bitmap_infoheader), 1, f_ptr);
+
+    // Move to beginning of image data
+    fseek(f_ptr, bfh.b_off_bits, SEEK_SET);
+    
+    // Allocate memory for image data
+    image = (unsigned char*)malloc(bih.bi_size_image);
+    if (!image) {
+        printf("Error: unable to allocate memory for image data.\n");
+        free(image);
+        fclose(f_ptr);
+        return EXIT_FAILURE;
+    }
+
+    // Read image data
+    fread(image, bih.bi_size_image, f_ptr);
+    if (image == NULL) {
+        printf("Error: unable to read image data\n");
+        fclose(f_ptr);
+        return EXIT_FAILURE;
+    }
+
+    fclose(f_ptr);
+    return EXIT_SUCCESS;
 }
--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/font.h
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/font.h
@@ -17,5 +17,31 @@ typedef struct font {
     int char_h;
 } font;
 
+#pragma pack(push, 1)
+typedef struct bitmap_fileheader {
+    unsigned short bf_type;     // File type
+    unsigned int bf_size;       // Byte size of the .bmp file
+    unsigned int bf_reserved;   // Reserved, must be 0
+    unsigned int b_off_bits;    // Specifies offset in bytes from file header 
+                                // to data section
+} bitmap_fileheader;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct bitmap_infoheader {
+    unsigned int bi_size; // Header size in bytes 
+    int bi_width; // Image width in pixels
+    unsigned int bi_height; // Image height in pixels
+    unsigned short bi_planes; // Number of color panes
+    unsigned short bi_bit_count; // Bits per pixel
+    unsigned int bi_compression; // Compression type
+    unsigned int bi_size_image; // Size of image in bytes
+    int bi_x_pels_per_meter; // Pixels/meter along x axis
+    int bi_y_pels_per_meter; // Pixels/meter along y axis
+    unsigned int bi_clr_used; // Number of colors used by the bitmap
+    unsigned int bi_clr_important; // Number of colors that are important
+} bitmap_infoheader;
+#pragma pack(pop)
+
 int init_fonts();
 int path_to_font(char* path, font* fontp); 
