63b33c7a5ca9ad5edb67574739861a7ed71dd2d1
Mathias Ose <mathias.ose@gmail.com>
2014-04-10 09:04:00 +0200

    pre-defined colors

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/graphics.h
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/graphics.h
@@ -12,6 +12,26 @@
 #include <sys/ioctl.h>
 #include "font.h"
 
+#define Black           0x0000
+#define Navy            0x000F
+#define DarkGreen       0x03E0
+#define DarkCyan        0x03EF
+#define Maroon          0x7800
+#define Purple          0x780F
+#define Olive           0x7BE0
+#define LightGrey       0xC618
+#define DarkGrey        0x7BEF
+#define Blue            0x001F
+#define Green           0x07E0
+#define Cyan            0x07FF
+#define Red             0xF800
+#define Magenta         0xF81F
+#define Yellow          0xFFE0
+#define White           0xFFFF
+#define Orange          0xFD20
+#define GreenYellow     0xAFE5
+#define Pink            0xF81F
+
 /* Prototypes */
 int init_framebuffer();
 void deinit_framebuffer();
