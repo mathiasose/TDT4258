f25958fea5d9e4898c4ff192b922b00c54fd1466
oyvindrobertsen <oyvind@arcticstyle.net>
2014-04-10 16:35:05 +0200

    bah

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/font.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/font.c
@@ -1,12 +1,12 @@
 #include "font.h"
 #define NUM_CHARS 91
 char* characters = {'\0', '!', '"', '#', '$', '%', '&', '|', '(', ')', '*', '+', ',', '-', '.',
-                    '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=',
-                    '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-                    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[',
-                    '\\', ']', '^', '_', '\'', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-                    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-                    'z'};
+    '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=',
+    '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[',
+    '\\', ']', '^', '_', '\'', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+    'z'};
 
 font* main_font;
 
@@ -27,7 +27,7 @@ int init_fonts()
 int path_to_font(char* path, font* fontp) 
 {
     FILE* f_ptr;
-    unsigned char* image;
+    uint32_t* image;
     bitmap_fileheader bfh;
     bitmap_infoheader bih;
     // Open file
@@ -35,6 +35,7 @@ int path_to_font(char* path, font* fontp)
         printf("Error: failed to open file.\n");
         return EXIT_FAILURE;
     }
+
 
     // Read file header
     fread(&bfh, sizeof(bitmap_fileheader), 1, f_ptr);
@@ -44,28 +45,53 @@ int path_to_font(char* path, font* fontp)
         return EXIT_FAILURE;
     }
 
-    // Read info header
+    //Read info header     
     fread(&bih, sizeof(bitmap_infoheader), 1, f_ptr);
 
-    // Move to beginning of image data
-    fseek(f_ptr, bfh.b_off_bits, SEEK_SET);
-    
-    // Allocate memory for image data
-    image = (unsigned char*)malloc(10);
+
+    printf("/*****************************************************/\n");
+    printf("/ IMAGE DEBUG DATA\n");
+    printf("/*****************************************************/\n");
+
+    printf("Type: %d\n", bfh.bf_type);
+    printf("File size: %d\n", bfh.bf_size);
+    printf("Reserved: %d\n", bfh.bf_reserved);
+    printf("Offset: %d\n", bfh.bf_off_bits);
+    printf("Infoheader size: %d\n", bih.bi_size);
+    printf("Width: %d, height: %d\n", bih.bi_width, bih.bi_height);
+    printf("Planes: %d\n", bih.bi_planes);
+    printf("BPP: %d\n", bih.bi_bit_count);
+    printf("Compression: %d\n", bih.bi_compression);
+    printf("BSI: %d\n", bih.bi_size_image);
+    printf("Xres: %d\n", bih.bi_x_pels_per_meter);
+    printf("Yres: %d\n", bih.bi_y_pels_per_meter);
+    printf("Used colors: %d\n", bih.bi_clr_used);
+    printf("Imp. colors: %d\n", bih.bi_clr_important);
+
+    int row_size = ((int)floor((bih.bi_bit_count * bih.bi_width + 31)/32)) * 4;
+    int image_data_size = row_size * abs(bih.bi_height);
+    printf("Image data size: %d\n", image_data_size);
+
+    image = (uint32_t*)malloc(image_data_size);
+
     if (image == NULL) {
-        printf("Error: unable to allocate memory for image data.\n");
-        free(image);
+        printf("Error: unable to allocate memory for image data\n");
         fclose(f_ptr);
         return EXIT_FAILURE;
     }
 
+    // Move to beginning of image data
+    fseek(f_ptr, bfh.bf_off_bits, SEEK_SET);
+
     // Read image data
-    fread(image, 10, 1,f_ptr);
+    fread(image, sizeof(image), 1, f_ptr);
     if (image == NULL) {
         printf("Error: unable to read image data\n");
         fclose(f_ptr);
         return EXIT_FAILURE;
     }
+
+    printf("Successfully loaded image into memory\n"); 
 
     // Populate fontp
     int char_idx;
@@ -73,8 +99,13 @@ int path_to_font(char* path, font* fontp)
     fontp->char_h = 24;
     for (char_idx = 0; char_idx < NUM_CHARS; char_idx++) {
         fontp->characters[char_idx].letter = characters[char_idx];
+        int char_x, char_y;
+        for (char_y = 0; char_y < char_h; char_y++) {
+            for (char_x = 0; char_x < char_w; char_x++) {
+
+            }
+        }
     }
-    
 
     fclose(f_ptr);
     return EXIT_SUCCESS;
--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/font.h
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/font.h
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct pixel_t {
     uint16_t colors;
@@ -17,31 +18,27 @@ typedef struct font {
     int char_h;
 } font;
 
-#pragma pack(push, 1)
-typedef struct bitmap_fileheader {
-    unsigned short bf_type;     // File type
-    unsigned int bf_size;       // Byte size of the .bmp file
-    unsigned int bf_reserved;   // Reserved, must be 0
-    unsigned int b_off_bits;    // Specifies offset in bytes from file header 
+typedef struct  __attribute__((__packed__)) bitmap_fileheader {
+    uint16_t bf_type;     // File type
+    uint32_t bf_size;       // Byte size of the .bmp file
+    uint32_t bf_reserved;   // Reserved, must be 0
+    uint32_t bf_off_bits;    // Specifies offset in bytes from file header 
                                 // to data section
 } bitmap_fileheader;
-#pragma pack(pop)
 
-#pragma pack(push, 1)
-typedef struct bitmap_infoheader {
-    unsigned int bi_size; // Header size in bytes 
-    int bi_width; // Image width in pixels
-    unsigned int bi_height; // Image height in pixels
-    unsigned short bi_planes; // Number of color panes
-    unsigned short bi_bit_count; // Bits per pixel
-    unsigned int bi_compression; // Compression type
-    unsigned int bi_size_image; // Size of image in bytes
-    int bi_x_pels_per_meter; // Pixels/meter along x axis
-    int bi_y_pels_per_meter; // Pixels/meter along y axis
-    unsigned int bi_clr_used; // Number of colors used by the bitmap
-    unsigned int bi_clr_important; // Number of colors that are important
+typedef struct __attribute__((__packed__)) bitmap_infoheader {
+    uint32_t bi_size; // Header size in bytes 
+    uint32_t bi_width; // Image width in pixels
+    uint32_t bi_height; // Image height in pixels
+    uint16_t bi_planes; // Number of color panes
+    uint16_t bi_bit_count; // Bits per pixel
+    uint32_t bi_compression; // Compression type
+    uint32_t bi_size_image; // Size of image in bytes
+    int32_t bi_x_pels_per_meter; // Pixels/meter along x axis
+    int32_t bi_y_pels_per_meter; // Pixels/meter along y axis
+    uint32_t bi_clr_used; // Number of colors used by the bitmap
+    uint32_t bi_clr_important; // Number of colors that are important
 } bitmap_infoheader;
-#pragma pack(pop)
 
 int init_fonts();
 int path_to_font(char* path, font* fontp); 
